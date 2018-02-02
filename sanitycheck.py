4c8ac5ad856e7f65a76acb44120741d137f92d8a
oneshell <mitchels.umd@gmail.com>
2018-02-02 11:53:05 -0500

    Create sanitycheck.py

---
+++ sanitycheck.py
@@ -0,0 +1,20 @@
+# ENME 489Y: Remote Sensing
+# Week 2: Introduction to Python & PyCharm
+
+import numpy as np
+import cv2
+import imutils
+
+print "All packages imported properly!"
+
+image = cv2.imread("testudo.jpg")
+
+cv2.imshow("Old School Testudo Logo: Original Dimensions", image)
+cv2.waitKey(0)
+
+image = imutils.resize(image, width=400)
+
+cv2.imshow("Old School Testudo Logo: Resized", image)
+cv2.waitKey(0)
+
+cv2.imwrite("testimage.jpg", image)
