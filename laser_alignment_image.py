03e0e200eed301b9952f44408ce7132e4884da72
Steve Mitchell <mitchels.umd@gmail.com>
2018-03-05 15:23:18 -0500

    Update laser_alignment_image.py

--- laser_alignment_image.py
+++ laser_alignment_image.py
@@ -17,18 +17,17 @@ rawCapture = PiRGBArray(camera, size=(1280,720))
 # allow the camera to setup
 time.sleep(1)
 
-# initialize image counter
-d = 0
-
-# open video feed and grab an image from the camera when desired
+# grab an image from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=False):
 
 	image = frame.array
+	# may need to flip image, depending on mechanical setup of instrument
 	image = cv2.flip(image,-1)
 
-	# plot crosshairs for alignment
+	# plot crosshairs, for alignment
 	cv2.line(image, (640,0), (640,720), (0,150,150), 1)
 	cv2.line(image, (0,360), (1280,360), (0,150,150), 1)
+	# plot green vertical lines, for alignment
 	for i in range(50, 1300, 50):
 		cv2.line(image, (i,0), (i, 720), (0,150,0), 3)
 
@@ -38,13 +37,10 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 	rawCapture.truncate(0)
 
+	# break out of video loop when specified by the user
 	if key == ord("q"):
 		break
 
-	if key == ord("p"):
-		filename = "file_%d.jpg" %d
 
-		# save each sequential d-indexed image to file
-		cv2.imwrite(filename, image)
-		d = d+1
+
 
